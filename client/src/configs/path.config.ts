ee0acf43bfcadb8d5f58d7912deed308b40d19d2
nhtrieu2k5 <nhtrieu2k5@gmail.com>
2024-10-16 15:44:35 +0700

    project

---
+++ client/src/configs/path.config.ts
@@ -0,0 +1,12 @@
+const path = {
+	home: '/',
+	product: '/product',
+	productDetail: '/product/:productId',
+	cart: '/cart',
+	login: '/login',
+	checkout: '/checkout',
+	register: '/register',
+	forgotPassword: '/forgot-password',
+};
+
+export default path;
