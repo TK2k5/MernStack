59b9e3915823b3b1e516a37396b03dd35b4ff51c
nhtrieu2k5 <nhtrieu2k5@gmail.com>
2024-11-06 08:16:08 +0700

    reducer tutorial

--- client/src/pages/cart/cart.tsx
+++ client/src/pages/cart/cart.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { Cart as CartType, TUpdateQuantityInCart } from "@/types/cart.type";
-import { ChevronRight, Minus, Plus, X } from "lucide-react";
+import { ChevronRight, Minus, Plus } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "@/stores/hooks";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { Button } from "@/components/ui/button";
-import { RootState } from "@/stores/store";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Separator } from "@/components/ui/separator";
 import { addToListCheckout } from "@/stores/features/cart/cart-slice";
 import { cartApi } from "@/api/cart.api";
@@ -14,16 +15,15 @@ import { formatCurrency } from "@/utils/format-currency.util";
 import { omit } from "lodash";
 import path from "@/configs/path.config";
 import { toast } from "sonner";
+import { useAppDispatch } from "@/stores/hooks";
 import { userApi } from "@/api/user.api";
+
+type CartItem = CartType & { checked: boolean };
 
 const Cart = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { carts: cartsListStore } = useAppSelector(
-    (state: RootState) => state.cart
-  );
-  console.log("🚀 ~ Cart ~ carts:", cartsListStore);
 
   const { data } = useQuery({
     queryKey: ["me"],
@@ -39,7 +39,21 @@ const Cart = () => {
   const total = responseCarts?.data?.total;
   const carts = responseCarts?.data?.carts;
 
-  const [cartItems, setCartItems] = useState<CartType[] | undefined>(carts);
+  const [cartItems, setCartItems] = useState<CartItem[] | undefined>([]);
+
+  // lấy ra những sản phẩm có checked là true
+  const checkedPurchases = useMemo(
+    () => cartItems?.filter((cart) => cart.checked),
+    [cartItems]
+  );
+
+  // tính tổng tiền các sản phẩm có checked là true
+  const totalCheckedPurchase = useMemo(() => {
+    const totalProductChecked = checkedPurchases?.reduce((total, purchase) => {
+      return total + purchase.quantity * purchase.productId.price;
+    }, 0);
+    return totalProductChecked;
+  }, [checkedPurchases]);
 
   const updateQuatityMutation = useMutation({
     mutationKey: ["update-quantity"],
@@ -71,9 +85,83 @@ const Cart = () => {
     });
   };
 
+  const handleCheckedChange = (value: boolean, productId: string) => {
+    const newCarts = cartItems?.map((cart) => {
+      if (cart._id === productId) {
+        return { ...cart, checked: value };
+      }
+      return cart;
+    });
+    setCartItems(newCarts);
+  };
+  /* ------------------------------------------------------------- */
+
+  /*
+  công dụng reduce
+  1. tính tổng(tổng số, tổng giá trị ....)
+  2. chuyển mảng thành một đối tượng
+  3. thực hiện các phép tính toán phức tạp
+  */
+
+  /*
+  cú pháp của reduce
+  array.reduce((accumulator, currentValue, currentIndex, array) => {}, initialValue)
+  1. accumulator: giá trị tích lũy (gía trị mà bạn muốn tính toán qua từng vòng lặp). Nó bắt đầu từ
+  initialValue và được cập nhật sau mỗi lần lặp
+  2. currentValue: giá trị của phần tử hiện tại trong mảng sau mỗi lần lặp
+  3. currentIndex: chỉ số của phần tử hiện tại trong mảng
+  4. array: mảng được duyệt
+  */
+
+  /*
+  const numbers = [1, 2, 3, 4, 5];
+
+  // let sum = 0; // initialValue
+  // for (let i = 0; i < numbers.length; i++) {
+  // 	sum += numbers[i]; // accumulator
+  // 	// numbers[i] // currentValue
+  // 	// i // currentIndex
+  // 	// numbers // array
+  // }
+
+  // console.log(sum);
+
+  /*
+  const total1 = numbers.reduce((accumulator, currentValue) => {
+    // vòng lặp 1: accumulator = 0, currentValue = 1 => return 0 + 1 = 1
+    // vòng lặp 2: accumulator = 1, currentValue = 2 => return 1 + 2 = 3
+    // vòng lặp 3: accumulator = 3, currentValue = 3 => return 3 + 3 = 6
+    // vòng lặp 4: accumulator = 6, currentValue = 4 => return 6 + 4 = 10
+    // vòng lặp 5: accumulator = 10, currentValue = 5 => return 10 + 5 = 15
+    return accumulator + currentValue;
+  }, 0);
+
+  /*
+  // chuyển mảng thành một đối tượng với các key là id và value là {id: number, name: string}
+  const users = [
+    { id: 1, name: "A" },
+    { id: 2, name: "B" },
+    { id: 3, name: "C" },
+  ];
+
+  /*
+  const resultUsers = users.reduce((accumulator, user) => {
+    accumulator[user.id] = user;
+    return accumulator;
+    // accumulator: là đổi tượng mình xây dựng trong quá trình lặp
+    // user.id: mỗi đối tượng user có thuộc tính id, và ta sử dụng id làm key trong obj
+    // accumulator[user.id] = user; => trở thành accumulator[1] = {id: 1, name: "A"} => {1: {id: 1, name: 'A}}
+    // accumulator[user.id] = user; => trở thành accumulator[2] = {id: 2, name: "B"} => {{1: {id: 1, name: 'A}}, 2: {id: 2, name: 'B'}}
+    // accumulator[user.id] = user; => trở thành accumulator[3] = {id: 3, name: "C"} => {{1: {id: 1, name: 'A}, 2: {id: 2, name: 'B'}, 3: {id: 3, name: 'C'}}
+  }, {});
+  console.log("🚀 ~ resultUsers ~ resultUsers:", resultUsers);
+
+  /* ------------------------------------------------------------- */
+
   useEffect(() => {
     if (carts) {
-      setCartItems(carts);
+      const purchases = carts.map((cart) => ({ ...cart, checked: false }));
+      setCartItems(purchases);
     }
   }, [carts]);
 
@@ -106,15 +194,26 @@ const Cart = () => {
           <div className="lg:w-2/3">
             {cartItems.map((item) => (
               <div key={item._id} className="flex items-center py-4 border-b">
+                <div className="mr-4">
+                  <Checkbox
+                    checked={item.checked}
+                    onCheckedChange={(value) => {
+                      handleCheckedChange(value as boolean, item._id);
+                    }}
+                  />
+                </div>
                 <img
                   src={item?.productId?.images[0]?.url}
                   alt={item?.productId?.nameProduct}
                   className="object-cover w-20 h-20 rounded"
                 />
                 <div className="flex-grow ml-4">
-                  <h3 className="font-semibold">
+                  <label
+                    htmlFor={`terms-${item._id}`}
+                    className="font-semibold"
+                  >
                     {item?.productId?.nameProduct}
-                  </h3>
+                  </label>
                   <p className="">Size: {item?.size}</p>
                   <div className="flex items-center gap-3">
                     <span className="">Color: </span>
@@ -170,15 +269,6 @@ const Cart = () => {
                     <Plus className="w-4 h-4" />
                   </Button>
                 </div>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  // onClick={() => removeItem(item.id)}
-                  className="ml-4"
-                  aria-label="Xóa sản phẩm"
-                >
-                  <X className="w-4 h-4" />
-                </Button>
               </div>
             ))}
           </div>
